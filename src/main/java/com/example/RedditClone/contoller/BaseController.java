24571da136af3872dae589b8018bc6ccd0b2024d
Pranit <pranitbhatt333@gmail.com>
2024-02-28 20:07:52 +0530

    Test

--- src/main/java/com/example/RedditClone/contoller/BaseController.java
+++ src/main/java/com/example/RedditClone/contoller/BaseController.java
@@ -1,6 +1,7 @@
 package com.example.RedditClone.contoller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class BaseController {
     @GetMapping("")
     public String getBaseController(){
         return "Reddit Home page Base Controller";
+    }
+
+    @PostMapping("")
+    public String postBaseController(){
+        return "Ans and and ";
     }
 }
